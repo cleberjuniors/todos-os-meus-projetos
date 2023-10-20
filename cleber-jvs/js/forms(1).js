f4904c7e579a264ec8ad34d1196ad4fd539a0ee9
cleberjuniors <128147342+cleberjuniors@users.noreply.github.com>
2023-10-20 08:22:32 -0300

    p

--- cleber-jvs/js/forms(1).js
+++ cleber-jvs/js/forms(1).js
@@ -1,33 +1,20 @@
-var botaoAdicionar = document.querySelector("#adicionar-paciente");
+var botaoAdicionar = document.querySelector("#buscar-paciente");
 botaoAdicionar.addEventListener("click", function(event) {
     event.preventDefault();
 
-    var form = document.querySelector("#form-adiciona");
+    var form = document.querySelector("#form-busca");
 
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
+    var nome = form.nome;
+    
 
     var pacienteTr = document.createElement("tr");
 
     var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
+    
 
     nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
 
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-
-    var tabela = document.querySelector("#tabela-pacientes");
+   
 
     tabela.appendChild(pacienteTr);
 
